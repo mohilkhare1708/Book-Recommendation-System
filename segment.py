0535aec9f1d3387c9608f7afebff89192545c03e
Mohil Khare <mohilkhare17@gmail.com>
2021-10-27 16:19:56 +0530

    segmentation for the call function and readme

---
+++ segment.py
@@ -0,0 +1,10 @@
+def BookRecommender(book_name):
+    book_list_name = []
+    book_id = df2[df2['title'] == book_name].index
+    book_id = book_id[0]
+    for newid in idlist[book_id]:
+        book_list_name.append(df2.loc[newid].title)
+    return book_list_name
+    
+BookNames = BookRecommender('Harry Potter and the Half-Blood Prince (Harry Potter  #6)')
+BookNames
